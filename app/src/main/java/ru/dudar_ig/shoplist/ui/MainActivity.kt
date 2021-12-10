2580eb1cf177715f20df40cd7390bcb7f70b990f
pakad <pakad@mail.ru>
2021-12-10 21:46:28 +0300

    1

--- app/src/main/java/ru/dudar_ig/shoplist/ui/MainActivity.kt
+++ app/src/main/java/ru/dudar_ig/shoplist/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package ru.dudar_ig.shoplist
+package ru.dudar_ig.shoplist.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.dudar_ig.shoplist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
